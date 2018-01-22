6d73e3419c6e6e86a0003008335b03d4a49516c3
jacklin <1272442383@qq.com>
2018-01-22 14:09:49 +0800

    update

--- common/constant.js
+++ common/constant.js
@@ -3,3 +3,7 @@
  */
 
 'use strict';
+
+module.exports = {
+ url: 'mongodb://localhost:27017'  // mongodb server
+};
--- demos/mongodb-crud.js
+++ demos/mongodb-crud.js
@@ -6,37 +6,17 @@
 
 var MongoClient = require('mongodb').MongoClient,
     assert = require('assert'),
-    cons = require('consolidate'),
-    express =  require('express'),
-    path = require('path'),
-    app = express();
+    constant =  require('../common/constant');
 
-// register the handlebars engine
-app.engine('handlebars', cons.handlebars);
-// set template engine
-app.set('view engine', 'handlebars');
-// template file des
-app.set('views', path.join(__dirname, '../views'));
+const dbName = 'video'; // db name
 
-var url = 'mongodb://localhost:27017';  // mongodb server
-var dbName = 'video'; // db name
-
-MongoClient.connect(url, function(err, client) {
+MongoClient.connect(constant.url, function(err, client) {
         assert.equal(null, err);
         console.log("Connected successfully to server");
-        const db = client.db(dbName);
 
-        app.get('/', function (req, res) {
-            // Find some documents in our collection
-            db.collection('movies', ()=>{console.log("callback")}).find({}).toArray(function(err, docs) {
-                res.render('movies', {'movies': docs});
-            });
-        });
-        app.use(function (req, res) {
-            res.sendStatus(404)
-        });
-        var server = app.listen(3000, function () {
-            console.log("express server listening at port 3000")
+        const db = client.db(dbName);
+        insertDocuments(db, function() {
+            client.close();
         });
     }
 );
@@ -88,3 +68,17 @@ MongoClient.connect(url, function(err, client) {
 //         }
 //     );
 // };
+
+const insertDocuments = function(collection, callback) {
+    // Insert some documents
+    collection.insertMany([
+        {a : 1}, {a : 2}, {a : 3}
+    ], function(err, result) {
+        assert.equal(err, null);
+        assert.equal(3, result.result.n);
+        assert.equal(3, result.ops.length);
+        console.log("Inserted 3 documents into the collection");
+        callback(result);
+    });
+};
+
